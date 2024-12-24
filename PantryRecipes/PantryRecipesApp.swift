87ddce6b366253a0a035b2fa695ab5ecef473df4
Jovanny Vasquez <jovannyvasquez15@gmail.com>
2024-12-24 18:50:17 -0500

    Initial Commit

---
+++ PantryRecipes/PantryRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryRecipesApp.swift
+//  PantryRecipes
+//
+//  Created by Jovanny Vasquez on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PantryRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
