1b4007b8a6f2c4212d8e1196e1c8113d31634041
Jovanny Vasquez <jovannyvasquez15@gmail.com>
2024-12-24 20:54:43 -0500

    Base app that takes a picture but no food classyfing capabilities

--- PantryRecipes/ContentView.swift
+++ PantryRecipes/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showImagePicker: Bool = false
+    @State private var selectedImage: UIImage?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //Title
+            Text("Welcome to Pantry Recipes")
+                .font(.title)
+                .padding()
+            
+            //Use imagepicker
+            if let image = selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .padding()
+            }
+            
+            //Button for taking a picture
+            Button(action:  {
+                showImagePicker = true
+            }) {
+                Text("Take Picture")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
         }
-        .padding()
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(selectedImage: $selectedImage, sourceType: .camera)
+        }
     }
 }
 
---
+++ PantryRecipes/FoodClassifier.swift
@@ -0,0 +1,8 @@
+//
+//  FoodClassifier.swift
+//  PantryRecipes
+//
+//  Created by Jovanny Vasquez on 12/24/24.
+//
+
+//Import CoreML model
